a9f3999a8c1543b94f2c85a0b42afd332c1fe1ed
Max Gemereth <177759368+gemerethmax@users.noreply.github.com>
2025-06-16 16:12:48 -0400

    cors and bodyParser middlewares set up. apiRouter mounted to app.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 module.exports = app;
 
@@ -10,13 +12,16 @@ app.use(express.static(__dirname))
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
-
+app.use(cors());
 
 // Add middware for parsing request bodies here:
+app.use(bodyParser.json());
 
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
+
+app.use('/api', apiRouter);
 
 
 // This conditional is here for testing purposes:
